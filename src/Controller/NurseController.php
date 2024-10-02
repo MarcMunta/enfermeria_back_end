997283382918bf188d0b5b93f91f3c23f95ac9f2
bryanjrr <bryanjoyarubio@gmail.com>
2024-10-02 17:02:58 +0200

    BuscarporNombreHecho

--- src/Controller/NurseController.php
+++ src/Controller/NurseController.php
@@ -4,11 +4,20 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
 
 class NurseController extends AbstractController
 {
+
+    public static $enfermeros = array(
+        array("Pepe54", "1234wsd", "Pepe"),
+        array("Pepe55", "1234wsd", "Pepe"),
+        array("PacoPlanchas", "patata", "Paco"),
+        array("PepitaLoca", "firulay", "Pepita"),
+        array("BenitoElGuay", "austrolopitecus", "Benito")
+    );
+
     #[Route('/lista_enfermeros', name: 'app_nurse')]
     public function index(): JsonResponse
     {
@@ -18,18 +27,38 @@ class NurseController extends AbstractController
         ]);
     }
 
-    public function importarJson($ruta){
 
-        $jsonData = file_get_contents($ruta);
-        $data = json_decode($jsonData, true);
+    #[Route('/nursebyname', name: 'app_nurse', methods: ['GET'])]
+    public function nursebyname(Request $request): JsonResponse
+    {
 
-        var_dump($data);
+        $length_enfermeros = count(self::$enfermeros);
 
+        for ($i = 0; $i < $length_enfermeros; $i++) {
+            if (self::$enfermeros[$i][0] == $request->get("user") && self::$enfermeros[$i][1] == $request->get("pass")) {
+
+                return new JsonResponse(true);
+            }
+        }
+
+        return new JsonResponse(false);
     }
 
+    #[Route('/buscarpNombre', name: 'app_nurse', methods: ['GET'])]
+    public function searchByName(Request $request): JsonResponse
+    {
 
+        $enfermerosTemp = [];
+        $enfermeroSize = count(self::$enfermeros);
 
+        for ($i = 0; $i < $enfermeroSize; $i++) {
+
+            if(self::$enfermeros[$i][2] == $request->get("nombre")){
+
+                array_push($enfermerosTemp, self::$enfermeros[$i][0]. " " . self::$enfermeros[$i][2]);
+            }
+        }
+
+        return new JsonResponse($enfermerosTemp);
+    }
 }
-
-
- 
